765f0a49e3bc910b7aec80b471a4222af1a5beb3
Rokiis1 <rokasnorviliss@gmail.com>
2022-12-04 01:24:42 +0200

    add account to JSON

--- api.go
+++ api.go
@@ -46,6 +46,8 @@ func (s *APIServer) Run()  {
 
 	router.HandleFunc("/account", makeHTTPHandleFunc(s.handleAccount))
 
+	router.HandleFunc("/account/{id}", makeHTTPHandleFunc(s.handleGetAccount))
+
 	log.Println("JSON API server running on port:", s.listenAddr)
 
 	http.ListenAndServe(s.listenAddr, router)
@@ -65,7 +67,12 @@ func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error 
 }
 
 func (s *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
-	return nil
+    id := mux.Vars(r)["id"]
+
+	// db.get(id)
+    fmt.Println(id)
+
+	return WriteJSON(w, http.StatusOK, &Account{})
 }
 
 func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
